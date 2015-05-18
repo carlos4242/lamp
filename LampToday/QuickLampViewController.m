8295115a9f75bcc8bc367b5b14032396d2cfa388
Carl Peto <carl@petosoft.com>
2015-05-18 05:30:02 +0100

    - fix bugs on notification center / today widget and add all on/off buttons

--- LampToday/QuickLampViewController.m
+++ LampToday/QuickLampViewController.m
@@ -16,6 +16,8 @@
 #define kLastResultLamp2On @"kLastResultLamp2On"
 #define kLastResultLamp3On @"kLastResultLamp3On"
 #define kLastResultSirenOn @"kLastResultSirenOn"
+#define kLastResultAnyOn @"kLastResultAnyOn"
+#define kLastResultNotAllOn @"kLastResultNotAllOn"
 
 @interface QuickLampViewController () <NCWidgetProviding>
 
@@ -30,12 +32,16 @@
 @property (weak, nonatomic) IBOutlet UILabel *arduinoNotContactable;
 @property (weak, nonatomic) IBOutlet UILabel *sirenLabel;
 @property (weak, nonatomic) IBOutlet UILabel *turnOnWifiLabel;
+@property (weak, nonatomic) IBOutlet UIButton *allOffButton;
+@property (weak, nonatomic) IBOutlet UIButton *allOnButton;
 
 @property (nonatomic) BOOL controlsVisible;
 @property BOOL lastResultLamp1On;
 @property BOOL lastResultLamp2On;
 @property BOOL lastResultLamp3On;
 @property BOOL lastResultSirenOn;
+@property BOOL lastResultAnyOn;
+@property BOOL lastResultNotAllOn;
 
 @property (strong) LampService *currentLampService;
 
@@ -45,10 +51,9 @@
 
 -(void)viewDidLoad {
     self.visualEffectView = [[UIVisualEffectView alloc] initWithEffect:[UIVibrancyEffect notificationCenterVibrancyEffect]];
-    self.visualEffectView.frame = CGRectMake(0, 0, self.view.frame.size.width, 200);
+    self.visualEffectView.frame = CGRectMake(0, 0, self.view.frame.size.width, 250);
+    self.preferredContentSize = CGSizeMake(self.view.frame.size.width,250);
     [self.view addSubview:self.visualEffectView];
-    NSLog(@"visual effect view %@",self.visualEffectView);
-
     [self.visualEffectView.contentView addSubview:self.roundLampLabel];
     [self.visualEffectView.contentView addSubview:self.tubeLampLabel];
     [self.visualEffectView.contentView addSubview:self.cornerLampLabel];
@@ -59,48 +64,50 @@
     [self.visualEffectView.contentView addSubview:self.sirenSwitch];
     [self.visualEffectView.contentView addSubview:self.arduinoNotContactable];
     [self.visualEffectView.contentView addSubview:self.turnOnWifiLabel];
+    [self.visualEffectView.contentView addSubview:self.allOffButton];
+    [self.visualEffectView.contentView addSubview:self.allOnButton];
+    self.allOffButton.layer.borderColor = [UIColor whiteColor].CGColor;
+    self.allOffButton.layer.borderWidth = 0.5;
+    self.allOffButton.layer.cornerRadius = 3;
+    self.allOnButton.layer.borderColor = [UIColor whiteColor].CGColor;
+    self.allOnButton.layer.borderWidth = 0.5;
+    self.allOnButton.layer.cornerRadius = 3;
     
     [self loadLastResults];
     [self setSwitchesEnabled];
     
     LampService *lamp = [self lampService:nil];
-    BOOL ran = [lamp checkState];
-    NSLog(@"widget view did load %d",ran);
+    [lamp checkState];
 }
--(void)setControlsVisible:(BOOL)controlsVisible {
-    [self setControlsVisible:controlsVisible withTransitionCoordinator:nil];
-}
--(void)setControlsVisible:(BOOL)visible
- withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator {
+-(void)setControlsVisible:(BOOL)visible {
     _controlsVisible = visible;
     BOOL onwifi = [LampService onWifi];
     void (^animations)() = ^{
-        self.tubeLampLabel.alpha = visible;
-        self.roundLampLabel.alpha = visible;
-        self.cornerLampLabel.alpha = visible;
-        self.sirenLabel.alpha = visible;
-        self.tubeLampSwitch.alpha = visible;
-        self.roundLampSwitch.alpha = visible;
-        self.cornerLampSwitch.alpha = visible;
-        self.sirenSwitch.alpha = visible;
-        self.arduinoNotContactable.alpha = !visible && onwifi;
-        self.turnOnWifiLabel.alpha = !visible && !onwifi;
+        self.tubeLampLabel.hidden = !visible;
+        self.roundLampLabel.hidden = !visible;
+        self.cornerLampLabel.hidden = !visible;
+        self.sirenLabel.hidden = !visible;
+        self.tubeLampSwitch.hidden = !visible;
+        self.roundLampSwitch.hidden = !visible;
+        self.cornerLampSwitch.hidden = !visible;
+        self.sirenSwitch.hidden = !visible;
+        self.allOnButton.hidden = !visible;
+        self.allOffButton.hidden = !visible;
+        self.arduinoNotContactable.hidden = visible || !onwifi;
+        self.turnOnWifiLabel.hidden = visible || onwifi;
     };
     
-    if (coordinator) {
-        [coordinator animateAlongsideTransition:^(id<UIViewControllerTransitionCoordinatorContext> context) {
-            animations();
-        } completion:nil];
-    } else {
-        animations();
-    }
-    self.preferredContentSize = CGSizeMake(self.view.frame.size.width,visible?200:100);
+    [UIView animateWithDuration:0.2 animations:animations];
 }
 -(void)setSwitchesEnabled {
     self.roundLampSwitch.on = self.lastResultLamp1On;
     self.tubeLampSwitch.on = self.lastResultLamp2On;
     self.cornerLampSwitch.on = self.lastResultLamp3On;
     self.sirenSwitch.on = self.lastResultSirenOn;
+    self.allOffButton.enabled = self.lastResultAnyOn;
+    self.allOffButton.alpha = self.allOffButton.enabled?1:0.2;
+    self.allOnButton.enabled = self.lastResultNotAllOn;
+    self.allOnButton.alpha = self.allOnButton.enabled?1:0.2;
 }
 -(void)updateSwitchesEnabled:(LampService*)service {
     if (service) {
@@ -108,12 +115,11 @@
         self.lastResultLamp2On = [service lampTwoIsOn];
         self.lastResultLamp3On = [service lampThreeIsOn];
         self.lastResultSirenOn = [service beedoBeedoIsOn];
+        BOOL anyOn = [service lampOneIsOn] || [service lampTwoIsOn] || [service lampThreeIsOn] || [service beedoBeedoIsOn];
+        BOOL allOn = [service lampOneIsOn] && [service lampTwoIsOn] && [service lampThreeIsOn];
+        self.lastResultAnyOn = anyOn;
+        self.lastResultNotAllOn = !allOn;
         [self setSwitchesEnabled];
-    }
-}
--(void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator {
-    if (!CGSizeEqualToSize(self.view.frame.size,size) && coordinator) {
-        [self setControlsVisible:_controlsVisible withTransitionCoordinator:coordinator];
     }
 }
 - (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult))completionHandler {
@@ -125,23 +131,13 @@
     self.currentLampService = lamp;
     __weak QuickLampViewController *weakSelf = self;
     lamp.completionFunction = ^(BOOL result,NSString *error) {
-        if ([LampService onHomeNetwork]) {
-            self.controlsVisible = YES;
-            weakSelf.preferredContentSize = CGSizeMake(self.view.frame.size.width,200);
-        } else {
-            self.controlsVisible = NO;
-            weakSelf.preferredContentSize = CGSizeMake(self.view.frame.size.width,100);
-        }
+        self.controlsVisible = [LampService onHomeNetwork];
         if (result) {
             [weakSelf updateSwitchesEnabled:weakSelf.currentLampService];
             [weakSelf saveLastResults];
-            if (completionHandler) {
-                completionHandler(NCUpdateResultNewData);
-            }
-        } else {
-            if (completionHandler) {
-                completionHandler(NCUpdateResultNewData);
-            }
+        }
+        if (completionHandler) {
+            completionHandler(NCUpdateResultNewData);
         }
         return YES;
     };
@@ -159,12 +155,20 @@
 - (IBAction)sirenSwitched:(id)sender {
     [[self lampService:nil] beedoBeedoSetState:self.sirenSwitch.on];
 }
+- (IBAction)allOnClicked:(id)sender {
+    [[self lampService:nil] allOn];
+}
+- (IBAction)allOffClicked:(id)sender {
+    [[self lampService:nil] allOff];
+}
 -(void)saveLastResults {
     [[NSUserDefaults standardUserDefaults] setBool:self.controlsVisible forKey:kLastResultVisible];
     [[NSUserDefaults standardUserDefaults] setBool:self.lastResultLamp1On forKey:kLastResultLamp1On];
     [[NSUserDefaults standardUserDefaults] setBool:self.lastResultLamp2On forKey:kLastResultLamp2On];
     [[NSUserDefaults standardUserDefaults] setBool:self.lastResultLamp3On forKey:kLastResultLamp3On];
     [[NSUserDefaults standardUserDefaults] setBool:self.lastResultSirenOn forKey:kLastResultSirenOn];
+    [[NSUserDefaults standardUserDefaults] setBool:self.lastResultAnyOn forKey:kLastResultAnyOn];
+    [[NSUserDefaults standardUserDefaults] setBool:self.lastResultNotAllOn forKey:kLastResultNotAllOn];
 }
 -(void)loadLastResults {
     self.controlsVisible = [[NSUserDefaults standardUserDefaults] boolForKey:kLastResultVisible];
@@ -172,6 +176,8 @@
     self.lastResultLamp2On = [[NSUserDefaults standardUserDefaults] boolForKey:kLastResultLamp2On];
     self.lastResultLamp3On = [[NSUserDefaults standardUserDefaults] boolForKey:kLastResultLamp3On];
     self.lastResultSirenOn = [[NSUserDefaults standardUserDefaults] boolForKey:kLastResultSirenOn];
+    self.lastResultAnyOn = [[NSUserDefaults standardUserDefaults] boolForKey:kLastResultAnyOn];
+    self.lastResultNotAllOn = [[NSUserDefaults standardUserDefaults] boolForKey:kLastResultNotAllOn];
 }
 
 @end
