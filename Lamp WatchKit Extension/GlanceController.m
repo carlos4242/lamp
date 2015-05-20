f6f2c4ad8c2de52bc05bf353603ed8115277d7e5
Carl Peto <carl@sonrnews.com>
2015-05-20 16:03:54 +0100

    - update watch app to handle error conditions better

--- Lamp WatchKit Extension/GlanceController.m
+++ Lamp WatchKit Extension/GlanceController.m
@@ -16,45 +16,56 @@
 
 @end
 
-
 @implementation GlanceController
 
 -(void)refreshGlance:(BOOL)show {
     [WKInterfaceController openParentApplication:@{@"action":@"refresh"} reply:^(NSDictionary *replyInfo, NSError *error) {
-        BOOL tubeOn = [replyInfo[@"tube"] boolValue];
-        BOOL roundOn = [replyInfo[@"round"] boolValue];
-        BOOL cornerOn = [replyInfo[@"corner"] boolValue];
-        BOOL bedoOn = [replyInfo[@"bedo"] boolValue];
-        BOOL anyOn = tubeOn||roundOn||cornerOn||bedoOn;
-        [self.mainStatus setText:anyOn?@"ON":@"OFF"];
         if (show) {
             [self.mainStatus setAlpha:1];
             [self.detailedStatus setAlpha:1];
         }
-        if (!anyOn) {
-            if (error) {
-                [self.detailedStatus setText:error.description];
-            } else {
-                [self.detailedStatus setText:@"All lamps are off"];
-            }
+        
+        if (error) {
+            [self.mainStatus setText:@""];
+            [self.detailedStatus setText:error.description];
         } else {
-            int lampCount = tubeOn+roundOn+cornerOn+bedoOn;
-            NSMutableString *lampsOn = [@"" mutableCopy];
-            if (tubeOn) {
-                [lampsOn appendString:@"tube,"];
+            NSString *problem = replyInfo[@"problem"];
+            if (problem) {
+                [self.mainStatus setText:@""];
+                [self.detailedStatus setText:problem];
+            } else {
+                BOOL tubeOn = [replyInfo[@"tube"] boolValue];
+                BOOL roundOn = [replyInfo[@"round"] boolValue];
+                BOOL cornerOn = [replyInfo[@"corner"] boolValue];
+                BOOL bedoOn = [replyInfo[@"bedo"] boolValue];
+                BOOL anyOn = tubeOn||roundOn||cornerOn||bedoOn;
+                
+                
+                if (anyOn) {
+                    [self.mainStatus setText:@"ON"];
+                    int lampCount = tubeOn+roundOn+cornerOn+bedoOn;
+                    NSMutableString *lampsOn = [@"" mutableCopy];
+                    if (tubeOn) {
+                        [lampsOn appendString:@"tube,"];
+                    }
+                    if (roundOn) {
+                        [lampsOn appendString:@"round,"];
+                    }
+                    if (cornerOn) {
+                        [lampsOn appendString:@"corner,"];
+                    }
+                    if (bedoOn) {
+                        [lampsOn appendString:@"flasher,"];
+                    }
+                    NSString *desc = [NSString stringWithFormat:@"\r\n%@ lamp%@ turned on",[lampsOn substringToIndex:lampsOn.length-1],lampCount>1?@"s are":@" is"];
+                    [self.detailedStatus setText:desc];
+                } else {
+                    [self.mainStatus setText:@"OFF"];
+                    [self.detailedStatus setText:@"All lamps are off"];
+                }
             }
-            if (roundOn) {
-                [lampsOn appendString:@"round,"];
-            }
-            if (cornerOn) {
-                [lampsOn appendString:@"corner,"];
-            }
-            if (bedoOn) {
-                [lampsOn appendString:@"flasher,"];
-            }
-            NSString *desc = [NSString stringWithFormat:@"\r\n%@ lamp%@ turned on",[lampsOn substringToIndex:lampsOn.length-1],lampCount>1?@"s are":@" is"];
-            [self.detailedStatus setText:desc];
         }
+
     }];
 }
 
