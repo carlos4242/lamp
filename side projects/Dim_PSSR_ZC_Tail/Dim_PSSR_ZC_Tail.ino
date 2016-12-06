3988a43da14d63fe4c15a191cc0cdccf754f79fb
Carl Peto <carlpeto@Carls-MacBook-Air.local>
2016-12-06 22:03:20 +0000

    reduce jitter and prevent serial comms from interfering with the dimmer timing

--- side projects/Dim_PSSR_ZC_Tail/Dim_PSSR_ZC_Tail.ino
+++ side projects/Dim_PSSR_ZC_Tail/Dim_PSSR_ZC_Tail.ino
@@ -1,13 +1,21 @@
 #include <EEPROM.h>
 #include <TimerOne.h>
 #include <stdlib.h>
+
 #define EEPROMUpdate(address,value) do {\
     byte current = EEPROM.read(address);\
     if (current != value) {\
       EEPROM.write(address,value);\
     }\
   } while (false);
-#define DEBUG_OUT(param) Serial.println(param)
+
+bool serialStarted = false;
+
+#define DEBUG_OUT(param) do {\
+    if (serialStarted) {\
+      Serial.println(param);\
+    }\
+  } while (false);
 
 
 /*
@@ -20,37 +28,35 @@
  1. Connect the C terminal of the ZeroCross Tail to digital pin 2 with a 10K ohm pull up to Arduino 5V.
  2. Connect the E terminal of the ZeroCross Tail to Arduino Gnd.
  3. Connect the PowerSSR Tail +in terminal to digital pin 4 and the -in terminal to Gnd.
+   http://bildr.org/2012/08/rotary-encoder-arduino/
 */
 
 #define saveLastTriggerPointAt 0x31
 #define saveLampOnAt 0x32
 #define minTriggerPoint 5
-#define maxTriggerPoint 120
+#define maxTriggerPoint 90
+#define brightnessStep 1
+#define brightnessMultiplier 10
+
 #define serialBufferSize 12 // the maximum length of the serial input/output
 // PowerSwitch Zero detect on this pin (must be interrupt capable)
 #define PZCD1 2
 // PowerSSR Tail connected to this digital pin to control power
 #define PSSR1 4
-// Set to 60hz mains
-#define freqStep 60
+// Set to 50hz mains
+#define freqStep 10
 
 
-// rotary encoder support:
+// rotary encoder support (including push switch):
 #define encoderPin1 7
 #define encoderPin2 8
-//push button switch
 #define encoderSwitchPin 3
 
-// dimmer state
-// level (0-128)  0 = on, 128 = off
-volatile int triggerPoint = minTriggerPoint;
 volatile boolean lampOn = true;
-
-// serial buffer/state and flags
-char serialBuffer[serialBufferSize];
-bool stateSaveNeeded = false;
-bool stateReportNeeded = false;
-
+volatile bool sentPulse = true; // this should start false!
+volatile int currentTriggerPoint;
+static int nextTriggerPoint;
+volatile boolean zero_cross = false; // Boolean to store a "switch" to tell us if we have crossed zero
 
 void setup()
 {
@@ -59,10 +65,16 @@ void setup()
 
   // fire a timer to count up after zero cross detected until the time when the triac should be fired
   Timer1.initialize(freqStep);
-  Timer1.attachInterrupt(triggerPoint_check, freqStep);
+  Timer1.attachInterrupt(timer_tick_function, freqStep);
 
   // Set SSR1 pin as output
   pinMode(PSSR1, OUTPUT);
+
+  // set zero cross detect pin as input and engage the pullup resistor
+  pinMode(PZCD1, INPUT);
+
+  pinMode(5,OUTPUT);
+  digitalWrite(5,LOW);
 
   // rotary encoder
   pinMode(encoderPin1, INPUT);
@@ -71,170 +83,206 @@ void setup()
   digitalWrite(encoderPin1, HIGH); //turn pullup resistor on
   digitalWrite(encoderPin2, HIGH); //turn pullup resistor on
   digitalWrite(encoderSwitchPin, HIGH);
-  attachInterrupt(digitalPinToInterrupt(encoderPin1), updateEncoder, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(encoderPin2), updateEncoder, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(encoderSwitchPin), encoderButtonPressed, CHANGE);
 
   // read most recent dimming level from EEPROM if available (virgin EEPROM address will read as 0xff)
-  triggerPoint = EEPROM.read(saveLastTriggerPointAt);
-  if (triggerPoint > maxTriggerPoint || triggerPoint < minTriggerPoint) {
-    triggerPoint = maxTriggerPoint;
+  int triggerPointRead = EEPROM.read(saveLastTriggerPointAt);
+
+  if (triggerPointRead > maxTriggerPoint || triggerPointRead < minTriggerPoint) {
+    triggerPointRead = maxTriggerPoint;
   }
+
+  nextTriggerPoint = triggerPointRead;
+  currentTriggerPoint = triggerPointRead;
+
   lampOn = EEPROM.read(saveLampOnAt);
-
-  // serial support
-  Serial.begin(9600);
-  memset(serialBuffer, 0, serialBufferSize);
-
-  // tell the world we are alive
-  DEBUG_OUT(F(">>Started"));
 }
 
 void writeStatus() {
+  if (!serialStarted) return;
+
+  static char outputSerialBuffer[serialBufferSize];
   if (lampOn) {
-    snprintf(serialBuffer, serialBufferSize, "DMR1=%d\n", triggerPoint);
+    snprintf(outputSerialBuffer, serialBufferSize, "DMR1=%d\n", currentTriggerPoint);
   } else {
-    snprintf(serialBuffer, serialBufferSize, "DMR1=_\n");
+    snprintf(outputSerialBuffer, serialBufferSize, "DMR1=_\n");
   }
-  Serial.write(serialBuffer);
+  Serial.write(outputSerialBuffer);
 }
 
-void turnOff() {
-  lampOn = false;
-}
-
-void turnOn() {
-  lampOn = true;
-}
-
-void encoderSwitchPressHandler() {
+void turnOff(bool * stateReportNeeded) {
   if (lampOn) {
-    turnOff();
-  } else {
-    turnOn();
+    lampOn = false;
+    *stateReportNeeded = true;
+    EEPROMUpdate(saveLampOnAt, lampOn);
   }
-
-  stateSaveNeeded = true;
-  stateReportNeeded = true;
-
-  attachInterrupt(digitalPinToInterrupt(encoderSwitchPin), encoderButtonPressed, CHANGE);
 }
 
-void interpretSerialCommand() {
+void turnOn(bool * stateReportNeeded) {
+  if (!lampOn) {
+    lampOn = true;
+    *stateReportNeeded = true;
+    EEPROMUpdate(saveLampOnAt, lampOn);
+  }
+}
+
+void interpretSerialCommand(char * serialBuffer, int * triggerPointPtr, bool * stateReportNeeded) {
+//  DEBUG_OUT(F("interpret...."));
+//  DEBUG_OUT(serialBuffer);
+  
   if (strncmp(serialBuffer, "DMR1:", 5) == 0) {
     char * command = serialBuffer + 5;
     if (*command == '?') {
-      stateReportNeeded = true;
+      *stateReportNeeded = true;
+//      DEBUG_OUT(F("report state"));
     } else if (*command == '_') {
-      turnOff();
-      stateSaveNeeded = true;
+      turnOff(stateReportNeeded);
+//      DEBUG_OUT(F("turn off"));
     } else if (*command == 'O') {
-      turnOn();
-      stateSaveNeeded = true;
+      turnOn(stateReportNeeded);
+//      DEBUG_OUT(F("turn on"));
     } else {
-      turnOn();
-      int newTriggerPoint = atoi(command);
-      if (newTriggerPoint > maxTriggerPoint) {
-        newTriggerPoint = maxTriggerPoint;
-      } else if (newTriggerPoint < minTriggerPoint) {
-        newTriggerPoint = minTriggerPoint;
+      turnOn(stateReportNeeded);
+//      DEBUG_OUT(F("change"));
+      int newTriggerPointVal = atoi(command);
+      if (newTriggerPointVal > maxTriggerPoint) {
+        newTriggerPointVal = maxTriggerPoint;
+      } else if (newTriggerPointVal < minTriggerPoint) {
+        newTriggerPointVal = minTriggerPoint;
       }
-      triggerPoint = newTriggerPoint;
-      stateSaveNeeded = true;
+      *triggerPointPtr = newTriggerPointVal;
     }
   }
 }
 
-volatile bool encoderSwitchPressed = false;
+void fireTriac() {
+  digitalWrite(PSSR1, HIGH);
+  delayMicroseconds(500);
+  digitalWrite(PSSR1, LOW);
+}
 
 void loop()
-{ // test with DMR1:?
-  static int serialBufferPosition = 0;
-  // check if serial debugging commands are available
-  if (Serial.available()) {
-    char c = Serial.read();
-    serialBuffer[serialBufferPosition] = c;
-    serialBufferPosition++;
-    if ((serialBufferPosition >= serialBufferSize) || (c == '\r')) {
-      // null terminate the string, interpret the command and reset the buffer
-      serialBuffer[serialBufferPosition] = 0;
-      interpretSerialCommand();
-      memset(serialBuffer, 0, serialBufferPosition);
-      serialBufferPosition = 0;
+{
+  static bool stateReportNeeded = false;
+
+  if (serialStarted) {
+    // test with DMR1:?
+    static int serialBufferPosition = 0;
+    // check if serial debugging commands are available
+    static char inputSerialBuffer[serialBufferSize];
+    if (Serial.available()) {
+      char c = Serial.read();
+      inputSerialBuffer[serialBufferPosition] = c;
+      serialBufferPosition++;
+      if ((serialBufferPosition >= serialBufferSize) || (c == '\r')) {
+        // null terminate the string, interpret the command and reset the buffer
+        inputSerialBuffer[serialBufferPosition] = 0;
+//        DEBUG_OUT(nextTriggerPoint);
+        interpretSerialCommand(inputSerialBuffer, &nextTriggerPoint, &stateReportNeeded);
+//        DEBUG_OUT(nextTriggerPoint);
+        serialBufferPosition = 0;
+      }
     }
   }
 
-  if (encoderSwitchPressed) {
-    encoderSwitchPressHandler();
-    encoderSwitchPressed = false; // reset flag
+  {
+    static int lastEncoded = 0;
+
+    int MSB = digitalRead(encoderPin1); //MSB = most significant bit
+    int LSB = digitalRead(encoderPin2); //LSB = least significant bit
+
+    int encoded = (MSB << 1) | LSB; //converting the 2 pin value to single number
+    int sum  = (lastEncoded << 2) | encoded; //adding it to the previous encoded value
+
+    if (sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) {
+
+      nextTriggerPoint += brightnessStep;
+
+      if (nextTriggerPoint > maxTriggerPoint) {
+        nextTriggerPoint = maxTriggerPoint;
+      }
+    }
+
+    if (sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) {
+
+      nextTriggerPoint -= brightnessStep;
+
+      if (nextTriggerPoint < minTriggerPoint) {
+        nextTriggerPoint = minTriggerPoint;
+      }
+    }
+
+    lastEncoded = encoded; //store this value for next time
   }
 
-  if (stateSaveNeeded) {
-    // save the eeprom update on the "main thread", in due course
-    EEPROMUpdate(saveLastTriggerPointAt, triggerPoint);
-    EEPROMUpdate(saveLampOnAt, lampOn);
-    stateSaveNeeded = false;
+  {
+    static int lastEncoderSwitchPinValue = false;
+    int encoderSwitchPinValue = digitalRead(encoderSwitchPin);
+
+    if (lastEncoderSwitchPinValue != encoderSwitchPinValue) {
+      delayMicroseconds(2000);
+      if (!encoderSwitchPinValue) {
+        // button is depressed
+        if (lampOn) {
+          turnOff(&stateReportNeeded);
+        } else {
+          turnOn(&stateReportNeeded);
+        }
+      }
+      lastEncoderSwitchPinValue = encoderSwitchPinValue;
+    }
   }
 
-  if (stateReportNeeded) {
-    writeStatus();
+  if (sentPulse) {
+
+    if (!serialStarted) {
+      Serial.begin(9600);
+      serialStarted = true;
+      DEBUG_OUT(F(">>Serial Started"));
+    }
+
+    if (currentTriggerPoint != nextTriggerPoint) {
+      currentTriggerPoint = nextTriggerPoint;
+      // save the eeprom update on the "main thread", in due course
+      EEPROMUpdate(saveLastTriggerPointAt, nextTriggerPoint);
+      stateReportNeeded = true;
+    }
+
+    if (stateReportNeeded) {
+      writeStatus();
+      stateReportNeeded = false;
+    }
+
+//    sentPulse = false; // this must reset to false!
   }
 }
 
 // ISRs
 // All these functions are ISRs, be wary of volatile variables, side effects and speed
 
-void updateEncoder() {
-  // http://bildr.org/2012/08/rotary-encoder-arduino/
-  volatile static int lastEncoded = 0;
-  int MSB = digitalRead(encoderPin1); //MSB = most significant bit
-  int LSB = digitalRead(encoderPin2); //LSB = least significant bit
+// Timer tick function.
+// This function will wait for zero cross, then after that, increment a counter on each tick until the proper time,
+// then finally fire the triac, reset the zero_cross flag and counter, ready for the next zero cross.
+volatile static int i = 0; // count the number of interrupts fired by the timer since the last zero cross
+void timer_tick_function() {
 
-  int encoded = (MSB << 1) | LSB; //converting the 2 pin value to single number
-  int sum  = (lastEncoded << 2) | encoded; //adding it to the previous encoded value
+  if (!lampOn) return;
 
-  if (sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) {
-    triggerPoint ++;
-    if (triggerPoint > maxTriggerPoint) {
-      triggerPoint = maxTriggerPoint;
-    }
-  }
-  if (sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) {
-    triggerPoint --;
-    if (triggerPoint < minTriggerPoint) {
-      triggerPoint = minTriggerPoint;
-    }
-  }
+  if (!zero_cross) return; // waiting for zero cross
 
-  lastEncoded = encoded; //store this value for next time
-}
-
-void encoderButtonPressed() {
-  detachInterrupt(digitalPinToInterrupt(encoderSwitchPin)); // avoid debounce
-  encoderSwitchPressed = true;
-}
-
-volatile boolean zero_cross = false; // Boolean to store a "switch" to tell us if we have crossed zero
-
-// This function will fire the triac at the proper time
-void triggerPoint_check() {
-  // First check to make sure the zero-cross has happened and the light should be on else do nothing
-  if (zero_cross && lampOn) {
-    volatile static int i = 0; // count the number of interrupts fired by the timer since the last zero cross
-    if (i >= triggerPoint) {
-      delayMicroseconds(100);        //These values will fire the PSSR Tail.
-      digitalWrite(PSSR1, HIGH);
-      delayMicroseconds(50);
-      digitalWrite(PSSR1, LOW);
-      i = 0;                         // Reset the counter
-      zero_cross = false;            // Reset the zero_cross so it may be turned on again at the next zero_cross_detect
-    } else {
-      i++;                           // If the trigger point has not been reached, increment the counter
-    }
+  if (i < currentTriggerPoint*brightnessMultiplier) {
+    i++;
+  } else {
+    zero_cross = false; // disable until the next zero cross
+    fireTriac();
+    sentPulse = true;
   }
 }
 
 void zero_cross_detect()
 {
+  i = 0;
   zero_cross = true;
+  sentPulse = false; // prevent main loop action from occurring until after next zero cross cycle is complete
+  digitalWrite(5,HIGH);
+  digitalWrite(5,LOW);
 }
